7c53d0278540d5c05afcb93e7e713dda7036dd1d
heyaplane <lowyieldgames@gmail.com>
2024-11-13 16:03:37 -0500

    Initial site layout

--- build_site.el
+++ build_site.el
@@ -51,7 +51,7 @@
       "<nav id=navbar>
         <ul>
          <li><a href=\"index.html\">Home</a></li>
-         <li><a href=\"index.html\">About</a></li>
+         <li><a href=\"about.html\">About</a></li>
         </ul>
        </nav>"
       )
